44caf9f0c83dd2da2c7ec4b766d7653f74b65383
Milena Kowalska <milenakowalska126@gmail.com>
2022-09-05 20:01:49 +0200

    Add sample test

--- finance_api/apps.py
+++ finance_api/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 class FinanceApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'finance_api'
--- finance_api/models.py
+++ finance_api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Animal(models.Model):
+    name = models.CharField(max_length=255)
+    sound = models.CharField(max_length=255)
+
+    def speak(self):
+        return f'The {self.name} says "{self.sound}"'
