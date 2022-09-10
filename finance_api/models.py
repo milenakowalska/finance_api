dcef90c2ae7aea56c36864403a3492983a37c34d
Milena Kowalska <milenakowalska126@gmail.com>
2022-09-10 21:38:35 +0200

    Add saving and recurring_saving models

--- finance_api/models.py
+++ finance_api/models.py
@@ -2,15 +2,24 @@ from datetime import date
 from django.db import models
 from django.contrib.auth.models import User
 
-class Contract(models.Model):
-    class Frequency(models.TextChoices):
+
+class Cost(models.Model):
+    name = models.CharField(max_length = 30)
+    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "%(class)s")
+    description = models.CharField(max_length = 300)
+    amount = models.FloatField(null = True)
+
+    class Meta:
+        abstract = True
+
+
+class Frequency(models.TextChoices):
         MONTHLY = 'MONTHLY'
         QUARTERLY = 'QUARTERLY'
         ANNUALY = 'ANNUALY'
 
-    name = models.CharField(max_length = 30)
-    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "contracts")
-    description = models.CharField(max_length = 300)
+
+class Contract(Cost):
     first_billing_day = models.DateField(default = date.today)
     end_date = models.DateField(null = True)
     billing_frequency = models.CharField(
@@ -18,4 +27,18 @@ class Contract(models.Model):
         choices = Frequency.choices,
         default = Frequency.MONTHLY
     )
-    billing_amount = models.FloatField(null = True)
+
+
+class Saving(Cost):
+    pay_out_day = models.DateField(null = True)
+
+
+class RecurringSaving(Cost):
+    start_date = models.DateField(default = date.today)
+    end_date = models.DateField(null = True)
+    billing_frequency = models.CharField(
+        max_length = 10,
+        choices = Frequency.choices,
+        default = Frequency.MONTHLY
+    )
+
