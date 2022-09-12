1fae3e6de37247d6d661ab76deab0cd87896e781
Milena Kowalska <milenakowalska126@gmail.com>
2022-09-12 20:38:53 +0200

    Implement pay_out_day and calculate stores amount for contracts

--- finance_api/models.py
+++ finance_api/models.py
@@ -7,16 +7,20 @@ class Cost(models.Model):
     name = models.CharField(max_length = 30)
     user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "%(class)s")
     description = models.CharField(max_length = 300)
-    amount = models.FloatField(null = True)
+    amount = models.FloatField(default=0)
 
     class Meta:
         abstract = True
 
 
 class Frequency(models.TextChoices):
-        MONTHLY = 'MONTHLY'
-        QUARTERLY = 'QUARTERLY'
-        ANNUALY = 'ANNUALY'
+    MONTHLY = 'MONTHLY'
+    QUARTERLY = 'QUARTERLY'
+    ANNUALY = 'ANNUALY'
+
+    @staticmethod
+    def frequency_to_months(frequency):
+        return 1 if frequency == Frequency.MONTHLY else 3 if frequency == Frequency.QUARTERLY else 12
 
 
 class Contract(Cost):
@@ -28,9 +32,49 @@ class Contract(Cost):
         default = Frequency.MONTHLY
     )
 
+    def archived(self):
+        return self.end_date < date.today() if self.end_date is not None else False
+
+    def compute_next_billing_day(self, reference_date = date.today()):
+        period_in_months = Frequency.frequency_to_months(self.billing_frequency)
+        billing_day = self.first_billing_day
+        while billing_day < reference_date:
+                billing_day = billing_day + relativedelta(months=+period_in_months)
+
+        return billing_day
+
+    def compute_amount_to_store(self, reference_date = date.today()):
+        period_in_months = Frequency.frequency_to_months(self.billing_frequency)
+        monthly_store = self.amount / period_in_months
+        next_billing = self.compute_next_billing_day(reference_date)
+        full_remaining_months = relativedelta(next_billing, reference_date).months
+        to_store = monthly_store * (period_in_months - full_remaining_months)
+        return to_store
+
+    """
+    10.03 - Quarterly, 2100 EUR
+    3 minus:
+    - 2 months x days -> 33%
+    - 1 month x days -> 66%
+    - 0 months x days -> 100%
+    10.06
+
+    10.03.2022 - Annually, 12000 EUR
+    12 minus:
+    - 11 months -> 1/12 * amount
+    - 10 months -> 1/12 * amount * 2
+    - 2 months x days -> 33%
+    - 1 month x days -> 66%
+    - 0 months x days -> 100%
+    10.03.2023
+    """
 
 class Saving(Cost):
     pay_out_day = models.DateField(null = True)
+
+    def paid_out(self):
+        return self.pay_out_day < date.today() if self.pay_out_day is not None else False
+
 
 
 class RecurringSaving(Cost):
@@ -43,17 +87,15 @@ class RecurringSaving(Cost):
         default = Frequency.MONTHLY
     )
 
-    def frequency_to_months(self):
-        return {
-        Frequency.MONTHLY: 1,
-        Frequency.QUARTERLY: 3,
-        Frequency.ANNUALY: 12
-        }[self.frequency]
+    def paid_out(self):
+        return self.pay_out_day < date.today() if self.pay_out_day is not None else False
 
     def saved_amount(self, reference_date):
         save_day = self.start_date
+        end_day = self.end_date if self.end_date is not None and self.end_date < reference_date else reference_date
         total_saved = 0
-        saving_period = self.frequency_to_months()
-        while save_day <= reference_date:
+        saving_period = Frequency.frequency_to_months(self.frequency)
+        while save_day <= end_day:
             total_saved += self.amount
             save_day = save_day + relativedelta(months=+saving_period)
+        return total_saved
--- finance_api/utils.py
+++ finance_api/utils.py
@@ -1,4 +1,5 @@
 from datetime import date
+from distutils import archive_util
 from .models import Contract, Saving, RecurringSaving
 from dataclasses import dataclass
 import calendar
@@ -7,12 +8,12 @@ import calendar
 class Statistics:
     balance: float
     savings: list
-    remaining_contracts: list
+    active_contracts: list
 
-    def __init__(self, balance, savings, remaining_contracts):
+    def __init__(self, balance, savings, active_contracts):
         self.balance = balance
         self.savings = savings
-        self.remaining_contracts = remaining_contracts
+        self.active_contracts = active_contracts
 
     def show(self):
         savings = []
@@ -21,11 +22,16 @@ class Statistics:
             savings.append(new_saving)
 
         contracts = []
-        for contract in self.remaining_contracts:
-            new_contract = {"name": contract.name, "billing_date": contract.billing_date, "amount": contract.amount}
+        for contract in self.active_contracts:
+            new_contract = {
+                "name": contract.name,
+                "billing_date": contract.billing_date,
+                "amount": contract.amount,
+                "stored_until_next_billing": contract.stored
+                }
             contracts.append(new_contract)
 
-        statistics_dict = {"disponible": self.balance, "savings": savings, "remaining_contracts": contracts}
+        statistics_dict = {"disponible": self.balance, "savings": savings, "active_contracts": contracts}
         return statistics_dict
 
 
@@ -47,39 +53,37 @@ class StatisticContract:
     billing_date: date
     amount: float
 
-    def __init__(self, name, billing_date, amount):
+    def __init__(self, name, billing_date, amount, stored):
         self.name = name
         self.billing_date = billing_date
         self.amount = amount
+        self.stored = stored
 
 
 def create_statistics(user, balance, reference_date):
     all_savings = []
-    remaining_contracts = []
+    active_contracts = []
     user_contracts = Contract.objects.all().filter(user = user)
     user_savings = Saving.objects.all().filter(user = user)
     user_recurring_savings = RecurringSaving.objects.all().filter(user = user)
 
     for saving in user_savings:
-        all_savings.append(StatisticSaving(saving.name, False, saving.amount))
-        balance -= saving.amount
+        if not saving.paid_out():
+            all_savings.append(StatisticSaving(saving.name, False, saving.amount))
+            balance -= saving.amount
 
     for recurring_saving in user_recurring_savings:
-        total_saved = recurring_saving.saved_amount(reference_date)
-        all_savings.append(StatisticSaving(recurring_saving.name, True, total_saved))
-        balance -= recurring_saving.amount
+        if not recurring_saving.paid_out():
+            total_saved = recurring_saving.saved_amount(reference_date)
+            all_savings.append(StatisticSaving(recurring_saving.name, True, total_saved))
+            balance -= recurring_saving.amount
 
     for contract in user_contracts:
-        try: 
-            contract_date = contract.first_billing_day
-            billing_day = date(reference_date.year, reference_date.month, contract_date.day)
-        except ValueError:
-            last_day = calendar.monthrange(reference_date.year, reference_date.month)[1]
-            billing_day = date(reference_date.year, reference_date.month, last_day)
+        if not contract.archived():
+            billing_day = contract.compute_next_billing_day(reference_date)
+            to_store = contract.compute_amount_to_store(reference_date)
 
-        if billing_day < reference_date:
-            amount = 0 if contract.amount is None else contract.amount
-            remaining_contracts.append(StatisticContract(contract.name, billing_day, amount))
-            balance -= amount
+            active_contracts.append(StatisticContract(contract.name, billing_day, contract.amount, to_store))
+            balance -= to_store
 
-    return Statistics(balance, all_savings, remaining_contracts)
+    return Statistics(balance, all_savings, active_contracts)
